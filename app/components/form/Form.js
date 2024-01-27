99adea3dc25f19d4991ccec364b08e6cb781a9ed
Ivan <ivanvolodin960@gmail.com>
2024-01-28 00:47:37 +0300

    improve: add full validation for email

--- app/components/form/Form.js
+++ app/components/form/Form.js
@@ -17,39 +17,48 @@ class Form {
 		const ROOT_ERROR = document.querySelector('.form__error')
 		const ROOT_BTN = document.querySelector('.form__btn')
 
+		const EMAIL_REGEXP =
+			/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu
+
+		const isEmailValid = value => {
+			return EMAIL_REGEXP.test(value)
+		}
+
 		const toggleErrorDisplay = () => {
+			if (isEmailValid(ROOT_INPUT.value)) {
+				ROOT_ERROR.style.display = 'none'
+				ROOT_BTN.classList.remove('btnNonActive')
+			}
+
 			if (ROOT_ERROR.style.display === 'none' && !ROOT_INPUT.value) {
 				ROOT_ERROR.style.display = 'block'
 				ROOT_BTN.classList.add('btnNonActive')
 			}
 
 			if (
-				ROOT_ERROR.style.display === 'block' &&
+				ROOT_ERROR.style.display === 'none' &&
 				ROOT_INPUT.value &&
-				ROOT_INPUT.value.includes('@')
+				!isEmailValid(ROOT_INPUT.value)
 			) {
-				ROOT_ERROR.style.display = 'none'
-				ROOT_BTN.classList.remove('btnNonActive')
+				ROOT_ERROR.style.display = 'block'
+				ROOT_BTN.classList.add('btnNonActive')
 			}
 		}
 
-		if (!ROOT_INPUT.value) {
-			ROOT_INPUT.addEventListener('input', toggleErrorDisplay)
-		}
+		ROOT_INPUT.addEventListener('input', toggleErrorDisplay)
 
-		if (ROOT_INPUT.value && ROOT_INPUT.value.includes('@')) {
+		if (ROOT_INPUT.value && isEmailValid(ROOT_INPUT.value)) {
 			ROOT_ERROR.style.display = 'none'
 			ROOT_INPUT.removeEventListener('input', toggleErrorDisplay)
 
-			// form.handlerClearForm()
 			form.renderSuccessForm(ROOT_INPUT.value)
 		}
 
-		if (!ROOT_INPUT.value && !ROOT_INPUT.value.includes('@')) {
+		if (!ROOT_INPUT.value) {
 			toggleErrorDisplay()
 		}
 
-		if (ROOT_INPUT.value && !ROOT_INPUT.value.includes('@')) {
+		if (ROOT_INPUT.value && !isEmailValid(ROOT_INPUT.value)) {
 			toggleErrorDisplay()
 		}
 	}
@@ -75,7 +84,7 @@ class Form {
         <h4 class="form__title">Начни прямо сейчас!</h4>
         <p class="form__text">Получи все нужные навыки для заработка на NFT всего за 28 дней!</p>
         <input class="form__input" type="email" placeholder="Ваш email" required />
-        <p class="form__error" style="display:none;">Поле ввода e-mail должно включать символ "@"</p>
+        <p class="form__error" style="display:none;">Введите корректный адрес электронной почты</p>
         <button class="form__btn shadow">Оплатить</button>
       </form>
     `
