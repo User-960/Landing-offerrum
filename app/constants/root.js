984bdabe31dd1910721c3dc959c6155360323ab9
Ivan <114345979+User-960@users.noreply.github.com>
2024-01-28 18:15:52 +0300

    Delete app directory

--- app/constants/root.js
+++
@@ -1,9 +0,0 @@
-/* Root elements */
-const ROOT_HEADER = document.querySelector('.header')
-const ROOT_FOOTER = document.querySelector('.footer')
-const ROOT_BENEFITS = document.querySelector('.benefits__box')
-
-const ROOT_FORM = document.querySelector('.form')
-const ROOT_SPINNER = document.querySelector('.spinner')
-
-const ROOT_TOP_BTN = document.querySelector('.top__btn')
